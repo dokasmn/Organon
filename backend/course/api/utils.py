56e0654972fd8e90f9a40a744fd05e4bbcb58d0a
doka43 <cauan_moreira@estudante.sesisenai.org.br>
2024-06-12 18:34:20 -0300

    conf: definindo primeiras rotas e organização do projeto

---
+++ backend/course/api/utils.py
@@ -0,0 +1,11 @@
+from ..models import *
+from .serializers import *
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    permission_classes = [IsAuthenticated]
