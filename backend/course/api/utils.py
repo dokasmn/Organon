e8dd12db19ae1724b93103c4b8ed6086774edab2
doka43 <cauan_moreira@estudante.sesisenai.org.br>
2024-06-13 17:06:17 -0300

    conf: configurando permissões de acesso

--- backend/course/api/utils.py
+++ backend/course/api/utils.py
@@ -2,8 +2,9 @@ from ..models import *
 from .serializers import *
 
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    permission_classes = [AllowAny] 
--- backend/perfil/api/utils.py
+++ backend/perfil/api/utils.py
@@ -2,7 +2,9 @@ from ..models import *
 from .serializers import *
 
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
     
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [AllowAny] 
