deb0ffb7bb0bbbe29f58125536f7a22e00c1c64a
Pedro Henrique Vitoreti <133826934+PHV00@users.noreply.github.com>
2024-06-13 13:47:01 -0300

    merge

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    body = models.TextField()
+# Create your models here.    
+class Materia(models.Model):
+    nome_materia = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f"Post: {self.title}"
+class Conteudo(models.Model):
+    nome_materia = models.CharField(max_length=100)
+    conteudo = models.CharField(max_length=1500)
+    fk_materia= models.ForeignKey(Materia, on_delete=models.CASCADE, null=False)
+    fk_professor = models
+
+class Usuario_Professor():
+    nome_professor = models.CharField(max_length=200)
