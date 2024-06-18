6180e835ae6db18922b856f111e24bb57500b1b3
doka43 <cauan_moreira@estudante.sesisenai.org.br>
2024-06-18 19:00:08 -0300

    conf: configurando opção de autenticação

--- backend/login/api/serializers.py
+++ backend/login/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from django.contrib.auth.models import User
 from ..models import Professor_user
 
 class ProfessorUserSerializer(serializers.ModelSerializer):
@@ -28,3 +30,41 @@ class ProfessorTokenObtainPairSerializer(TokenObtainPairSerializer):
         except Professor_user.DoesNotExist:
             professor_user = None
         return data
+
+class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        max_length=128,
+        write_only=True,
+    )
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError("Invalid email or password")
+
+            if not user.check_password(password):
+                raise serializers.ValidationError("Invalid email or password")
+
+            attrs['user'] = user
+        else:
+            raise serializers.ValidationError("Must include 'email' and 'password'.")
+
+        return super().validate(attrs)
+
+    def validate_email(self, value):
+        return value.lower()  # Convertendo o email para minúsculas
+
+    def validate_password(self, value):
+        return value
--- backend/requests_tests/login/auth_user.py
+++ backend/requests_tests/login/auth_user.py
@@ -1,38 +1,36 @@
 import requests
-import json
-import os
 
-def auth_user(username, password):  
+def auth_user(email, password):  
     response = requests.post('http://localhost:8000/login/api/token/', data={
-        'username': username,
+        'email': email,
         'password': password,
     })
+    print(response)
     tokens = response.json()
+    print(tokens)
     if 'access' in tokens:
         print("Você está logado")
     else:
-        print("Usuário não encontrado")
+        print("Usuário não encontrado ou senha incorreta")
     return tokens.get('access', None)
 
 def get_user_profile(access_token):
-    response = requests.get('http://localhost:8000/login/api/get_user_data/', headers={
+    headers = {
         'Authorization': f'Bearer {access_token}'
-    })
-    print(response)
+    }
+    response = requests.get('http://localhost:8000/login/api/get_user_data/', headers=headers)
     datas = response.json() 
+    print(response.status_code)
+    if response.status_code == 200:
+        print(datas)
+    else:
+        print("Erro ao obter perfil do usuário")
     return datas
 
-def print_response(title, response):
-    print("="*30 + f" {title} " + "="*30)
-    try:
-        print(response)
-    except Exception as e:
-        print(f"Erro ao imprimir a resposta: {e}")
-        print(response)
-        
-os.system('cls')
-username = input("username: ")
-password = input("password: ")
-my_token = auth_user(username,password)
-my_datas = get_user_profile(my_token)
-print_response("perfil",my_datas)
+email = input("Email: ")
+password = input("Password: ")
+
+my_token = auth_user(email, password)
+
+if my_token:
+    get_user_profile(my_token)
