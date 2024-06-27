184a4070ca97156dc725d08f121174c4b33ae8d2
Pedro Henrique Vitoreti <133826934+PHV00@users.noreply.github.com>
2024-06-27 15:00:50 -0300

    conf: models

--- backend/login/models.py
+++ backend/login/models.py
@@ -43,20 +43,22 @@ class CustomUser(AbstractBaseUser):
         self.confirmation_code_created_at = timezone.now()
         self.save()
 
-class Professor_user(models.Model):
-    professor_auth_user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)    
-    class Meta:
-        ordering = ['professor_auth_user']
-        verbose_name = 'Professor'
-        verbose_name_plural = 'Professores'
-
 class Profession(models.Model):
     profession_name = models.CharField(max_length=45)
+
+class Academic_Education(models.Model):
+    degree = models.CharField(max_length=30)
+    training_name = models.CharField(max_length=30)
 
 class Professional_History(models.Model):
     company = models.CharField(max_length=40,verbose_name="Compania/Empresa")
     fk_profession = models.ForeignKey(Profession, on_delete=models.CASCADE,verbose_name="Professor",null=True,blank=True)
 
-class Academic_Education(models.Model):
-    degree = models.CharField(max_length=30)
-    training_name = models.CharField(max_length=30)
+class Professor_user(models.Model):
+    professor_auth_user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)    
+    fk_academic_education = models.ForeignKey(Academic_Education, on_delete= models.CASCADE,verbose_name="Academic Formation",null=True,blank=True)
+    fk_Professional_History = models.ForeignKey(Professional_History, on_delete=models.CASCADE,verbose_name="Professional History",null=True,blank=True)
+    class Meta:
+        ordering = ['professor_auth_user']
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
