83f66861445d58c01a9f611e9019215cbeb68509
daniel <daniellimafreita@gmail.com>
2024-07-02 16:09:48 -0300

    corrigindo axios do login

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 // REACT
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../axiosConfig';
 
